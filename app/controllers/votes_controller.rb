ce2d2a182c02dd22c4ef600aa98d6dd079e9fbe3
Andrew Klein <andrew.klein.j@gmail.com>
2016-10-08 15:31:39 -0400

    add logic to destroy vote if user clicks vote button for response they already voted for

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,19 @@
 class VotesController < ApplicationController
 
   def create
-    vote = Vote.create(vote_params)
-    vote.user = current_user
-    vote.save
-    redirect_to root_path
+    if current_user.liked_response_ids.include?(vote_params[:response_id].to_i)
+      Vote.find_by(response_id: vote_params[:response_id], user_id: session[:user_id]).destroy
+      redirect_to root_path
+    else
+      vote = Vote.create(vote_params)
+      vote.user = current_user
+      vote.save
+      redirect_to root_path # should revise -- this redirects user to current
+      # prompt show page even if they voted through the prompt index page.
+      # Perhaps voting should not be possible on prompt index page? Or prompt
+      # index page should not render the current prompt (so the index page is
+      # PAST decrees)
+    end
   end
 
   # def destroy
