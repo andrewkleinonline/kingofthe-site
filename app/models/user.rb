d5a560fdb39912f5e91c5004d53bedc1ad525dc8
Andrew Klein <andrew.klein.j@gmail.com>
2016-10-08 15:54:24 -0400

    add #crown method to User class to change instance to new king

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,11 @@ class User < ApplicationRecord
     self.liked_response_ids.include?(response_id)
   end
 
+  def crown
+    User.current_king.update(king: false)
+    self.update(king: true)
+  end
+
 
   def vote_button_text(response)
     response.voter_ids.include?(self.id) ? "Unvote" : "Vote"
